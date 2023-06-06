4efa4c6350fec79dda6e4925f670a03a70bad140
Deva <deva.thevaranchan@gmai.com>
2023-06-06 18:15:33 +0200

    header

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-white"> 
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+   
+  <ul class="navbar-nav mr-auto">
+     <div class="navbarcontent">
+    <li class="nav-item active">
+        <a class="nav-link" href="aPropos.php">a Propos de KayLak <span class="sr-only"></span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Menu</a>
+      </li>
+
+
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Nos plats </a>
+      </li>
+
+      <li class="nav-iem">
+        <a class="nav-link" href= "index.php" ><img class = "logo" src="img/KayLek2.png" alt="Description de l'image" ></a>
+      </li>
+
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Nos Formations </a>
+      </li>
+
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Contactez nous </a>
+      </li>
+
+      <li class="">
+        <a class="nav-link" href="#"><img class = "login" src="img/LOGIN.png" alt="Description de l'image" > <div class="compte" >COMPTE</div> </a>
+      </li>
+    </div>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
