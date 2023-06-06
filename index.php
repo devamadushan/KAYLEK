3326cf3e3c79bd56daf5c8337d470f829cfd7330
Deva <deva.thevaranchan@gmai.com>
2023-06-06 21:29:03 +0200

    Les conditions

--- index.php
+++ index.php
@@ -41,6 +41,28 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+    <br>
+    <h1>Nos Conditions</h1>
+    <div class="lesconditions">
+    
+   
+    
+    </div>
+    
+
+    <table class="tables">
+  
+    <tr>
+      <th ><img class="condition" src="img/cuisson.gif"></th>
+      <th scope="col"> <img class="condition" src="img/heures-douverture.gif"></th>
+      <th scope="col"><img class="condition" src="img/route.gif" ></th>
+    </tr>
+    <tr>
+      <td style="text-align: center;">Nous cuisinons uniquement à la demande <br> minimum de commande 4plats/jus</td>
+      <td style="text-align: center;">Commande à passer 72h avant</td>
+      <td style="text-align: center;">Retrait de commande possible a la Gare de Savigny</td>
+    </tr>
+</table>
+  
 </body>
 </html>
