ca9454db2d89d52090eb8b9f986c7aa8445e42a7
Deva <deva.thevaranchan@gmai.com>
2023-06-09 15:17:30 +0200

    page nos plats

--- nosplats.php
+++ nosplats.php
@@ -13,29 +13,31 @@
 <body>
     <div class="nosplats">
     <img class="NPimg" src="img/nosplats1.png">
-    <h1>Plats fait maison</h1>
-    <table>
+    <h1 class="NPtitre">Plats fait maison</h1>
+    <table class="NPlist">
         <tr>
     <th>Sénegalais</th><th>végétariens</th><th>Salé & Sucré</th>
     </tr>
     </table>
     </div>
-    <h1>Les plats Sénégalais</h1>
+    <h1 class="platsenetitre">Les plats Sénégalais</h1>
+    <div class="platsene">
+    
 <table>
     <tr>
         <td>
-    <h2>Thiebou yapp</h2>
-    <p>
+    <h2 class="platsenesotitre">Thiebou yapp</h2>
+    <p  class="platsenesotitre">
 Spécialité du Sénégal, qui veut dire riz à la viande. Tieb signifie riz et yapp viande.
 
 Composé de principalement de riz et de viande. Ainsi que des légumes, oignon, poivron vert,  gousse d’ail. Déclinable sous plusieurs versions en fonction des personnes.
 
 Chez Kaylek, nous vous proposons 2 versions, bœuf ou poulet.</p>
 </td>
-<td><img class="" src="img/nosplats2.png"></td>
+<td><img class="nosplatsimg" src="img/nosplats2.png"></td>
 <tr>
-    <td><img class="" src="img/nosplats3.jpg"></td>
-    <td><h2>Yassa</h2> <p>Plat à base d’oignon frits et de riz qui peut être accompagné de viande marinée dans le citron ou de poisson.
+    <td><img class="nosplatsimg" src="img/nosplats3.jpg"></td>
+    <td><h2 class="platsenesotitre" >Yassa</h2> <p class="platsenesotitre">Plat à base d’oignon frits et de riz qui peut être accompagné de viande marinée dans le citron ou de poisson.
 
 Yassa à la viande ou Yassa au poisson.
 
@@ -44,28 +46,29 @@ C’est un plat simple et rapide à préparer généralement très apprécié av
 Chez Kaylek, nous proposons le Yassa au poulet.</p></td>
 </tr>
 
-<tr><td><h2>Mafé</h2> <p>Plat à base de viande ou poisson peut aussi être végétarien, cuit dans une sauce au beurre de cacahuète et servi avec du riz
+<tr><td><h2 class="platsenesotitre" >Mafé</h2> <p class="platsenesotitre" >Plat à base de viande ou poisson peut aussi être végétarien, cuit dans une sauce au beurre de cacahuète et servi avec du riz
 
 Kaylek prépare le Mafé au poulet ou bœuf en fonction de vos envie. Ainsi qu’au légume pour les végétariens.</p>
 </td>
-<td><img class="" src="img/nosplats4.jpg"></td>
+<td><img class="nosplatsimg" src="img/nosplats4.jpg"></td>
 </tr>
 <tr>
-    <td><img class="" src="img/nosplats5.png"></td>
-<td><h2>Pastel</h2><p>Délicieux beignet frits farcis à la viande ou poisson très populaire au Sénégal.
+    <td><img class="nosplatsimg" src="img/nosplats5.png"></td>
+<td><h2 class="platsenesotitre" >Pastel</h2><p class="platsenesotitre" >Délicieux beignet frits farcis à la viande ou poisson très populaire au Sénégal.
 
 Kaylek propose des pastels au thon ou poulet.</p></td>
 </tr>
 <tr>
-    <td><h2>Thiacry</h2><p>Petite douceur sucré. Très apprécié en Afrique de l’Ouest. Fait à base de couscous de mil (céréale provenant des régions sèche) et du yaourt.
+    <td><h2 class="platsenesotitre" >Thiacry</h2><p class="platsenesotitre">Petite douceur sucré. Très apprécié en Afrique de l’Ouest. Fait à base de couscous de mil (céréale provenant des régions sèche) et du yaourt.
 
 Chez Kaylek, le Thiacry est fait à partir de semoule et de fromage blanc.</p></td>
-<td><img class="" src="img/nosplats6.png"></td>
+<td><img class="nosplatsimg" src="img/nosplats6.png"></td>
 </tr>
 </table>
-
-<img class="" src="img/nosplats7.jpg">
-<h1>Boissons</h1>
+</div>
+<div class="boisson">
+<img class="boitimg" src="img/nosplats7.jpg">
+<h1 class="boititre">Boissons</h1>
 <table>
     <tr>
         <td><h2>Jus de bissap</h2></td>
@@ -78,40 +81,46 @@ Chez Kaylek, le Thiacry est fait à partir de semoule et de fromage blanc.</p></
         <td>Boisson traditionnel au Sénégal. Appelée aussi Gnamankouji qui veut dire ” eau pimentée ” fait référence au jus de gingembre. Rafraichissante et piquante. Fait à base de gingembre cette boisson est réputée pour ses vertus sur la santé</td>
     </tr>
 </table>
-
+</div>
+<h1 class="PVtitre">Les plats végétariens</h1>
+<div class="pvegetarien">
 <table>
     <tr>
-        <td><h1>Les plats végétariens</h1><h2>Thièp</h2><p>A l’origine du Thiéboudiène. Plat à base de riz et de poissons.
+        <td><h2 class="vegcont">Thièp</h2><p class="vegcont">A l’origine du Thiéboudiène. Plat à base de riz et de poissons.
 
 Kaylek propose sa version végétarien composée principalement de légume frais.</p></td>
-<td><img class="" src="img/nosplats8.png"></td>
+<td><img class="PVimg" src="img/nosplats8.png"></td>
     </tr>
     <tr>
         <td>
-        <img class="" src="img/nosplats9.png">
+        <img class="PVimg" src="img/nosplats9.png">
         </td>
-        <td><h2>Vermicelles</h2><p>Plat familiale à base de vermicelle devenu au fil des ans un plat de fête au Sénégal. Décliner sous plusieurs formes, viande ou végétarien.
+        <td><h2 class="vegcont">Vermicelles</h2><p class="vegcont">Plat familiale à base de vermicelle devenu au fil des ans un plat de fête au Sénégal. Décliner sous plusieurs formes, viande ou végétarien.
 
 Kaylek vous proposes sa version végé aux 5 légumes.</p></td>
     </tr>
-    <tr><td><h2>Thiou</h2><p>Thiou qui signifie “ragout”. Comme l’indique son nom c’est un ragout généralement de viande ou de poissons.
+    <tr><td><h2 class="vegcont">Thiou</h2><p class="vegcont">Thiou qui signifie “ragout”. Comme l’indique son nom c’est un ragout généralement de viande ou de poissons.
 
 Chez Kaylek le Thiou est à base de tomate concentrée et de toute sorte de légume.</p></td>
-<td><img class="" src="img/nosplats10.png"></td>
+<td><img class="PVimg" src="img/nosplats10.png"></td>
 </tr>
 </table>
+</div>
+
+<div class="sucsal">
 <table>
 <tr>
-    <td><h1>Salé</h1><p>En plus de nos pastels.
+    <td><h1 class="sucsaltitre">Salé</h1><p class="sucsalstitre">En plus de nos pastels.
 
 Kaylek propose divers plateaux au choix ou à composer</p></td>
-<td><h1>Sucré</h1><p>text....</p></td>
+<td><h1 class="sucsaltitre">Sucré</h1><p class="sucsalstitre">text....</p></td>
 </tr>
 
     <tr>
         
-<td><img class="" src="img/nosplats11.JPg"></td>
-<td><img class="" src="img/nosplats11.JPg"></td>
+<td><img class="sucsalimg" src="img/nosplats11.JPg"></td>
+<td><img class="sucsalimg" src="img/nosplats11.JPg"></td>
 </tr>
 </table>
+</div>
 </body>
